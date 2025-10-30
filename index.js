5b08fb389d36699b6ef5602932075bb867b13fd4
Yemi.dev <opeyemiodusanya60@gmail.com>
2025-10-29 22:00:27 -0700

    The open weather map api

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-//
+//Using the open weather map api to fetch weather data for a given city
+const apiKey = 'YOUR_API_KEY_HERE           
+
+const fetchWeather = async (city) => {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+    );
+    const data = await response.json();
+    return data;
+}
+export default fetchWeather;
+
